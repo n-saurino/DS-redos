3d3e862d511a6853cb57d37fffdf298f576c2c90
jsaurino <nsaurino@smu.edu>
2015-12-20 01:41:06 -0600

    Initial Commit

---
+++ LaserTag/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LaserTag
+//
+//  Created by Nigel Saurino on 12/20/15.
+//  Copyright © 2015 Nigel Saurino. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
